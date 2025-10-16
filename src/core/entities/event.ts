179502dd56ae6e0626eb1f2e35e9a9c313f42d93
Victor Taveira <rodrigueseti@outlook.com>
2025-10-16 11:50:20 -0300

    refactor: remove course and event entities, course repository, and console UI

--- src/core/entities/event.ts
+++
@@ -1,7 +0,0 @@
-import type { Course } from "./course";
-
-export interface Event {
-  title: string;
-  date: string;
-  courses: Course[];
-}
--- src/core/interfaces/course-repository.ts
+++
@@ -1,5 +0,0 @@
-import type { Event } from "../entities/event";
-
-export interface ICourseRepository {
-  getEvents(): Promise<Event[]>;
-}
